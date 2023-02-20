4dbe54d263c368589d79cc432b5159626b740938
rkddusen <k99endus@naver.com>
2023-02-20 22:33:44 +0900

    edit key evaluation

--- src/component/KeyBoard.js
+++ src/component/KeyBoard.js
@@ -12,17 +12,23 @@ function KeyBoard(props) {
       _arrWord[0] = props.word[0];
       let _arrResult = [];
       _arrResult[0] = props.result[0];
-      
+
       let isin = false;
       for (let i = 1; i < 5; i++) {
         for (let j = 0; j < _arrWord.length; j++) {
           if (props.word[i] === _arrWord[j]) {
             // 중복 값이 있을 때
             isin = true;
-            if (_arrResult[j] === 'O' && ( props.result[i] === 'B' || props.result[i] === 'S')) {
-              if(_arrResult[j] === 'B' &&  props.result[i] === 'S')
-                // 기존 결과보다 더 정확한 결과
-                _arrResult[j] = props.result[i];
+            console.log(_arrResult[j]);
+            console.log(props.result[i]);
+            if (
+              _arrResult[j] === "O" &&
+              (props.result[i] === "B" || props.result[i] === "S")
+            ) {
+              // 기존 결과보다 더 정확한 결과
+              _arrResult[j] = props.result[i];
+            } else if (_arrResult[j] === "B" && props.result[i] === "S") {
+              _arrResult[j] = props.result[i];
             }
             break;
           }
@@ -31,30 +37,35 @@ function KeyBoard(props) {
           _arrWord.push(props.word[i]);
           _arrResult.push(props.result[i]);
         } else {
-          isin = false
+          isin = false;
         }
       }
 
-
-      if (key.length < 1) { // key에 아무것도 없을 때
-        console.log('시작')
+      if (key.length < 1) {
+        // key에 아무것도 없을 때
+        console.log("시작");
         setKey(_arrWord);
         setValue(_arrResult);
-      } else { // key와의 중복 체크
-        console.log('중복체크')
+      } else {
+        // key와의 중복 체크
+        console.log("중복체크");
         for (let i = 0; i < _arrWord.length; i++) {
-          let index = key.findIndex((element) => element === _arrWord[i])
-          if(index !== -1){ // 중복 있음
-            if(value[index] ==='O'){
-              if(_arrResult[i] === 'S' || _arrResult[i] === 'B'){ // 새로 추가되는 결과가 더 좋을 때
+          let index = key.findIndex((element) => element === _arrWord[i]);
+          if (index !== -1) {
+            // 중복 있음
+            if (value[index] === "O") {
+              if (_arrResult[i] === "S" || _arrResult[i] === "B") {
+                // 새로 추가되는 결과가 더 좋을 때
                 value[index] = _arrResult[i];
               }
-            } else if(value[index] ==='B'){
-              if(_arrResult[i] === 'S'){ // 새로 추가되는 결과가 더 좋을 때
+            } else if (value[index] === "B") {
+              if (_arrResult[i] === "S") {
+                // 새로 추가되는 결과가 더 좋을 때
                 value[index] = _arrResult[i];
               }
             }
-          } else { // 중복 없음
+          } else {
+            // 중복 없음
             key.push(_arrWord[i]);
             value.push(_arrResult[i]);
           }
@@ -64,7 +75,6 @@ function KeyBoard(props) {
       }
     }
   }, [props.word, props.result]);
-
 
   return (
     <div className="keyboard">
